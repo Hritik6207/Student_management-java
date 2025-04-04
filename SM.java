b3aa5c665d26c2ae0515fdec816e7208460b3a0f
Hritik Kumar Sinha <103347828+Hritik6207@users.noreply.github.com>
2025-04-04 10:30:38 +0530

    Create SM.java

---
+++ SM.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+class Student {
+    String name;
+    int roll;
+    float[] marks = new float[3];
+
+    void input(Scanner sc) {
+        System.out.print("Enter name: ");
+        name = sc.next();
+        System.out.print("Enter roll number: ");
+        roll = sc.nextInt();
+        System.out.print("Enter marks in 3 subjects: ");
+        for (int i = 0; i < 3; i++) {
+            marks[i] = sc.nextFloat();
+        }
+    }
+
+    float getAverage() {
+        float sum = 0;
+        for (float mark : marks) {
+            sum += mark;
+        }
+        return sum / 3;
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll: " + roll);
+        System.out.println("Average Marks: " + getAverage());
+    }
+}
+
+public class StudentManager {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        Student[] students = new Student[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nEnter details for student " + (i + 1));
+            students[i] = new Student();
+            students[i].input(sc);
+        }
+
+        System.out.println("\n--- Student Details ---");
+        for (Student s : students) {
+            s.display();
+            System.out.println("------------------------");
+        }
+
+        sc.close();
+    }
+}
